047899eb063cc5eafe54198ed0ca95c3fdfc0547
artur martinho <arturmartinhobr2004@gmail.com>
2024-03-04 14:35:13 -0300

    load imagem correto

--- jogo.py
+++ jogo.py
@@ -5,26 +5,23 @@ def inicializa():
     tela = pygame.display.set_mode((1000, 700))
     pygame.display.set_caption('jogo Artur e Matheus')
     assets = {}
-    lista1 = []
-    lista2 = []
-    lista3 = []
-    lista4 = []
-    assets['coracao'] = pygame.image.load('fotos pygame\Captura de tela 2024-03-04 135754.png')
-    assets['pocao'] = pygame.image.load('fotos pygame\Captura de tela 2024-03-04 140048.png')
-    assets['bau'] = pygame.image.load('fotos pygame\Captura de tela 2024-03-04 140440.png')
-    assets['chave'] = pygame.image.load('fotos pygame\Captura de tela 2024-03-04 140618.png')
-    assets['raio'] = pygame.image.load('fotos pygame\Captura de tela 2024-03-04 140735.png')
-    assets['maca'] = pygame.image.load('fotos pygame\Captura de tela 2024-03-04 140842.png')
+    assets['coracao'] = pygame.image.load('fotos pygame\coracao.png')
+    assets['pocao'] = pygame.image.load('fotos pygame\pocao.png')
+    assets['bau'] = pygame.image.load('fotos pygame\bau.png')
+    assets['chave'] = pygame.image.load('fotos pygame\chave.png')
+    assets['raio'] = pygame.image.load('fotos pygame\raio.png')
+    assets['maca'] = pygame.image.load('fotos pygame\maca.png')
 
     
-    return tela
+    return tela, assets
 
 game = True
 
 
 
-def desenha(tela):
+def desenha(tela,assets):
     tela.fill((255, 255, 255))
+    tela.blit(())
     
     
     pygame.display.update()
