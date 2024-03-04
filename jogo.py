e8a221e4a7d1b517aedc9edf25f8e90484982e0e
artur martinho <arturmartinhobr2004@gmail.com>
2024-03-04 14:23:01 -0300

    assets

--- jogo.py
+++ jogo.py
@@ -9,6 +9,13 @@ def inicializa():
     lista2 = []
     lista3 = []
     lista4 = []
+    assets['coracao'] = pygame.image.load('fotos pygame\Captura de tela 2024-03-04 135754.png')
+    assets['pocao'] = pygame.image.load('fotos pygame\Captura de tela 2024-03-04 140048.png')
+    assets['bau'] = pygame.image.load('fotos pygame\Captura de tela 2024-03-04 140440.png')
+    assets['chave'] = pygame.image.load('fotos pygame\Captura de tela 2024-03-04 140618.png')
+    assets['raio'] = pygame.image.load('fotos pygame\Captura de tela 2024-03-04 140735.png')
+    assets['maca'] = pygame.image.load('fotos pygame\Captura de tela 2024-03-04 140842.png')
+
     
     return tela
 
@@ -17,7 +24,7 @@ game = True
 
 
 def desenha(tela):
-    tela.fill((250, 250, 250))
+    tela.fill((255, 255, 255))
     
     
     pygame.display.update()
@@ -28,7 +35,7 @@ def game_loop(tela):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-
+        desenha(tela)
 
 if __name__ == '__main__':
     tela = inicializa()
